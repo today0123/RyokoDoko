c8b98123dcccb20b03167c2313ee6a5e25d91bdf
Rayvinia <hyeonju96@gmail.com>
2018-11-09 17:30:18 +0900

    RyokoDoko

--- app/src/main/java/com/ryokodoko/ryokodoko/ChecklistActivity.java
+++ app/src/main/java/com/ryokodoko/ryokodoko/ChecklistActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
@@ -15,8 +16,8 @@ import com.google.android.gms.common.images.ImageRequest;
 
 public class ChecklistActivity extends AppCompatActivity {
 
-    Toolbar toolbarChecklist;
-    LinearLayout iLChecklist;
+    android.support.v7.widget.Toolbar toolbarChecklist;
+    RelativeLayout rLChecklist;
     CoordinatorLayout cLChecklist;
     ImageView iVNothingTodo;
     TextView tVNotingTodo;
@@ -26,7 +27,18 @@ public class ChecklistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
-
         Intent intent = getIntent();
+
+        toolbarChecklist = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbarChecklist);
+        rLChecklist = (RelativeLayout) findViewById(R.id.rLChecklist);
+        cLChecklist = (CoordinatorLayout) findViewById(R.id.cLchecklist);
+        iVNothingTodo = (ImageView) findViewById(R.id.iVNothingTodo);
+        tVNotingTodo = (TextView) findViewById(R.id.tVNothingTodo);
+        fabChecklist = (FloatingActionButton) findViewById(R.id.fabChecklist);
+
+        toolbarChecklist.setTitle(getResources().getString(R.string.checklist));
+        toolbarChecklist.setTitleTextColor(getResources().getColor(R.color.headerStyle));
+        setSupportActionBar(toolbarChecklist);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
