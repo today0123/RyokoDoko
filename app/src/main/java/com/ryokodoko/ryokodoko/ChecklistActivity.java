a39819d45f241a63c73ed12fa27413e016942f80
Rayvinia <hyeonju96@gmail.com>
2018-11-14 00:14:29 +0900

    RyokoDoko

--- app/src/main/java/com/ryokodoko/ryokodoko/ChecklistActivity.java
+++ app/src/main/java/com/ryokodoko/ryokodoko/ChecklistActivity.java
@@ -1,15 +1,24 @@
 package com.ryokodoko.ryokodoko;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.gms.common.images.ImageRequest;
@@ -40,5 +49,41 @@ public class ChecklistActivity extends AppCompatActivity {
         toolbarChecklist.setTitleTextColor(getResources().getColor(R.color.headerStyle));
         setSupportActionBar(toolbarChecklist);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_checklist_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_delete_checklist:
+                //삭제 알림창 속성 설정
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(getResources().getString(R.string.notice_delete_all)).setCancelable(false)
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Ok", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                //알림창 객체 생성
+                dialog.show();
+                //알림창 띄우기
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
